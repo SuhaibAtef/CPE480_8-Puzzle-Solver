652d0906643452ffd5be119e86750c6e55f66545
Suhaib Atef <suhaibatef20@gmail.com>
2022-04-07 16:48:07 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,84 @@
+from collections import deque
+from prettytable import PrettyTable
+from node import Node
+
+generatedStates =1 
+expandedStates =0
+goalState = ''
+
+def solvable(initState):
+    def getInvCount(arr):
+        inv_count = 0
+        for i in range(0, 9):
+            for j in range(i + 1, 9):
+                if arr[j] != '0' and arr[i] != '0' and arr[i] > arr[j]:
+                    inv_count += 1
+        return inv_count
+
+    invCount = getInvCount(list(initState))
+    return (invCount % 2 == 0)
+
+def solveDFS(initState,goalState):
+    global expandedStates
+    global generatedStates
+    queue = deque()
+    path = [] 
+    currentNode = Node(initState)
+    while not currentNode.nIsGoal(goalState):
+        print(currentNode.state)
+        currentNode.expandNode()
+        expandedStates += 1
+        queue.extendleft(currentNode.children)
+        generatedStates+=len(currentNode.children)
+        currentNode = queue.pop()
+
+    path.append(currentNode.state)
+    for parent in currentNode.getParents():
+        path.append(parent.state)
+    path.reverse()
+
+    return path
+
+def printState(state):
+    z=state.find('0')
+    state = list(state)
+    state[z]=" "
+    stateTable= PrettyTable()
+    stateTable.border = False
+    stateTable.header = False
+    stateTable.field_names = ["Col1","Col2","Col3"]
+    stateTable.add_row([state[0],state[1],state[2]])
+    stateTable.add_row([state[3],state[4],state[5]])
+    stateTable.add_row([state[6],state[7],state[8]])
+    print(stateTable)
+
+
+def generateGoalState(state):
+    goalState = sorted(state)
+    goalState = ''.join(goalState)
+    return goalState
+
+def main():
+    global generatedStates 
+    global expandedStates 
+    initialState = ""
+    while(len(initialState)!=9):
+        initialState = input("Please insert your 8 puzzle string:")
+    
+    printState(initialState)
+    global goalState
+    goalState = generateGoalState(initialState)
+    printState(goalState)
+    if(solvable(initialState)):
+        path = solveDFS(initialState,goalState)
+        for node in path:
+            printState(node)
+            print("\n"*2)
+        print("Generated States:" + str(generatedStates)+"\n")
+        print("Expanded States:" + str(expandedStates)+"\n")
+    else:
+        print("cannot be solved")
+    
+
+if __name__ == "__main__":
+    main()
---
+++ node.py
@@ -0,0 +1,90 @@
+
+
+import this
+
+
+def stateInAction(state):
+    ActionsAllowed = []
+    zeroPosition = state.find('0')
+    def ActionRight(state,index):
+        state = list(state)
+        newState = state.copy()
+        newState[index] = state[index+1]
+        newState[index+1] = state[index]
+        newState = "".join(newState)
+        ActionsAllowed.append(newState) 
+        
+    def ActionLeft(state,index):
+        state = list(state)
+        newState = state.copy()
+        newState[index] = state[index-1]
+        newState[index-1] = state[index]
+        newState = "".join(newState)
+        ActionsAllowed.append(newState)
+
+    def ActionUp(state,index):
+        state = list(state)
+        newState = state.copy()
+        newState[index] = state[index-3]
+        newState[index-3] = state[index]
+        newState = "".join(newState)
+        ActionsAllowed.append(newState)
+
+    def ActionDown(state,index):
+        state = list(state)
+        newState = state.copy()
+        newState[index] = state[index+3]
+        newState[index+3] = state[index]
+        newState = "".join(newState)
+        ActionsAllowed.append(newState) 
+
+    if(zeroPosition!=0 and zeroPosition!=1 and zeroPosition!=2):
+        ActionUp(state,zeroPosition)
+
+    if(zeroPosition!=6 and zeroPosition!=7 and zeroPosition!=8):
+        ActionDown(state,zeroPosition)
+        
+    if(zeroPosition!=2 and zeroPosition!=5 and zeroPosition!=8):
+        ActionRight(state,zeroPosition)
+
+    if(zeroPosition!=0 and zeroPosition!=3 and zeroPosition!=6):
+        ActionLeft(state,zeroPosition)
+        
+    return ActionsAllowed
+    
+
+def isGoal(state,goalState):
+    for i in range(len(state)):
+            if state[i] != goalState[i]:
+                return False
+    return True
+
+
+class Node:
+
+    def __init__(self, state=None,parent=None,depth=0,children=[]):
+      self.state = state
+      self.parent = parent
+      self.depth = depth
+      self.children = children
+
+    def getState(self):
+        return self.state
+
+    def nIsGoal(self,goalState):
+        return isGoal(self.state, goalState)
+
+    def expandNode(self):
+        newStates = stateInAction(self.state)
+        self.children = []
+        for state in newStates:
+            self.children.append(Node(state, self, self.depth + 1))
+
+    def getParents(self):
+        currentNode = self
+        while currentNode.parent:
+            yield currentNode.parent
+            currentNode = currentNode.parent
+
+
+
