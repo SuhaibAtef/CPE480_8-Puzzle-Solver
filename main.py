209ebe9d8c95c955618339999bb7c0ded63d383c
Suhaib Atef <saalhindi18@cit.just.edu.jo>
2022-04-09 13:07:53 +0300

    added the option to test at expand or generate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from collections import deque
+from secrets import choice
 from prettytable import PrettyTable
 from node import Node
 
@@ -6,19 +7,41 @@ generatedStates =1
 expandedStates =0
 goalState = ''
 
-def solveDFS(initState,goalState):
+def solveDFS(initState,goalState,choice):
     global expandedStates
     global generatedStates
     queue = deque()
     path = [] 
     currentNode = Node(initState)
-    while not currentNode.nIsGoal(goalState):
-        ##print(currentNode.state)
-        currentNode.expandNode()
-        expandedStates += 1
-        queue.extendleft(currentNode.children)
-        generatedStates+=len(currentNode.children)
-        currentNode = queue.pop()
+
+    if (currentNode.nIsGoal(goalState)):
+        path.append(currentNode)
+        return path
+
+    if (choice==1):
+        while not currentNode.nIsGoal(goalState) :
+            #print(currentNode.state)
+                currentNode.expandNode()
+                expandedStates += 1
+                generatedStates+=len(currentNode.children)
+                for child in currentNode.children:
+                    if(child.nIsGoal(goalState)):
+                        path.append(child)
+                        for parent in child.getParents():
+                            path.append(parent)
+                        path.reverse()
+                        return path
+                queue.extendleft(currentNode.children)
+                currentNode = queue.pop()
+    
+    if (choice==2):
+        while not currentNode.nIsGoal(goalState):
+            ##print(currentNode.state)
+            currentNode.expandNode()
+            expandedStates += 1
+            queue.extendleft(currentNode.children)
+            generatedStates+=len(currentNode.children)
+            currentNode = queue.pop()
 
     path.append(currentNode)
     for parent in currentNode.getParents():
@@ -51,21 +74,29 @@ def main():
     global expandedStates 
     initialState = ""
     actions = []
+    choice =-1
     while(len(initialState)!=9):
         initialState = input("Please insert your 8 puzzle string:")
-    
+    while(choice!=1 and choice!=2):
+        choice = int(input("1: Test at node generation 2: Test at node expanding, what's your choice?"))
+    print("===== Initial State =====")
     printState(initialState)
+    print("===== ===== ===== =====\n")
     global goalState
     goalState = generateGoalState(initialState)
+    print("===== Goal State =====")
     printState(goalState)
-    path = solveDFS(initialState,goalState)
+    print("===== ===== ===== =====\n")
+
+    print("======= Moves =======\n")
+    
+    path = solveDFS(initialState,goalState,choice)
     for node in path:
         printState(node.state)
         print("\n"*2)
         actions.append(node.action)
-
+    print("===== ===== ===== =====")
     print ("Actions:" + str(actions)[5:-1] )
-
     print("Generated States:" + str(generatedStates))
     print("Expanded States:" + str(expandedStates))
     
